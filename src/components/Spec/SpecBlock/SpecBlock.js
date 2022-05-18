293ed83cc269768ce55bdfc2170b1415e1355b0e
Anton Holub <golubanton753@gmail.com>
2022-05-18 15:36:19 +0300

    fix spec blocks

--- src/components/Spec/SpecBlock/SpecBlock.js
+++ src/components/Spec/SpecBlock/SpecBlock.js
@@ -3,18 +3,12 @@ import styles from './specblock.module.css';
 
 const SpecBlock = ({img, text}) => {
     const [bgSpec, setBgSpec] = useState({
-        background: `linear-gradient(0deg, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(${img})`,
+        background: `url(${img})`,
     });
     return (
         <div
             className={styles.img} 
-            style={{...bgSpec}}
-            onMouseOver={() => setBgSpec({
-                background: `linear-gradient(0deg, rgba(0, 95, 163, 0.65), rgba(0, 95, 163, 0.65)), url(${img})`
-            })}
-            onMouseOut={() => setBgSpec({
-                background: `linear-gradient(0deg, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(${img})`
-            })}>
+            style={{...bgSpec}}>
             <div className={styles.text}>
                 {text}
             </div>
