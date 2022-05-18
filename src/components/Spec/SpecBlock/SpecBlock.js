227ac091b8262dc2f7f0762376d67ebab870841b
Anton Holub <golubanton753@gmail.com>
2022-05-18 16:17:32 +0300

    fix cards

--- src/components/Spec/SpecBlock/SpecBlock.js
+++ src/components/Spec/SpecBlock/SpecBlock.js
@@ -2,19 +2,18 @@ import { useState } from 'react';
 import styles from './specblock.module.css';
 
 const SpecBlock = ({img, text}) => {
-    const [bgSpec, setBgSpec] = useState({
-        background: `linear-gradient(0deg, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(${img})`,
-    });
+    const [bgSpec, setBgSpec] = useState(styles.img);
     return (
         <div
-            className={styles.img} 
-            style={{...bgSpec}}
-            onMouseOver={() => setBgSpec({
-                background: `linear-gradient(0deg, rgba(0, 95, 163, 0.65), rgba(0, 95, 163, 0.65)), url(${img})`
-            })}
-            onMouseOut={() => setBgSpec({
-                background: `linear-gradient(0deg, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(${img})`
-            })}>
+            className={bgSpec} 
+            style={{background: `url(${img})`}}
+            onMouseOver={() => {
+                setBgSpec(styles.img_hover)
+            }}
+            onMouseOut={() => {
+                setBgSpec(styles.img)
+            }}
+            >
             <div className={styles.text}>
                 {text}
             </div>
